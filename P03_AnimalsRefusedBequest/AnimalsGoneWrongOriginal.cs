acb5a4d382884232891a7a54b7ffe34a474977f9
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2022-01-04 23:08:13 +0200

    Added the third problem - AnimalsGoneWrong

---
+++ P03_AnimalsRefusedBequest/AnimalsGoneWrongOriginal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_AnimalsGoneWrong
+{
+    public class AnimalsGoneWrongOriginal
+    {
+        public class Animal
+        {
+            public string Breed { get; set; }
+
+            public Animal(string breed)
+            {
+                Breed = breed;
+            }
+
+            protected virtual void Walk() 
+            {
+                Console.WriteLine("An animal is walking");
+            }
+        }
+
+        public class Cat : Animal 
+        {
+            public string Name { get; set; }
+
+            public Cat(string breed, string name)
+                : base(breed)
+            {
+                Name = name;
+            }
+
+            protected override void Walk()
+            {
+                Console.WriteLine($"The cat {Name} is walking");
+            }
+        }
+
+        public class Fish : Animal 
+        {
+            public Fish(string breed)
+                : base (breed)
+            {
+
+            }
+
+            protected override void Walk()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ P03_AnimalsRefusedBequest/AnimalsGoneWrongRefactored.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_AnimalsGoneWrong
+{
+    public class AnimalsGoneWrongRefactored
+    {
+        public class Animal
+        {
+            public string Breed { get; set; }
+
+            public Animal(string breed)
+            {
+                Breed = breed;
+            }
+
+        }
+
+        public class Walker : Animal
+        {
+            public Walker(string breed)
+                : base(breed)
+            {
+                Breed = breed;
+            }
+
+            protected virtual void Walk() 
+            {
+                Console.WriteLine("Waling");
+            }
+        }
+
+        public class Swimmer : Animal 
+        {
+            public Swimmer(string breed)
+                : base(breed)
+            {
+
+            }
+        }
+
+        public class Cat : Walker
+        {
+            public string Name { get; set; }
+
+            public Cat(string breed, string name)
+                : base(breed)
+            {
+                Name = name;
+            }
+
+            protected override void Walk()
+            {
+                Console.WriteLine($"The cat {Name} is walking");
+            }
+        }
+
+        public class Fish : Swimmer
+        {
+            public Fish(string breed)
+                : base(breed)
+            {
+
+            }
+        }
+    }
+}
