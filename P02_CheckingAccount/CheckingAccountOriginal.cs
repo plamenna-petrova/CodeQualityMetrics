2efe4041d890fe35addbb46dd3de688ae631b3fc
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2022-01-04 21:13:53 +0200

    Added second problem - CheckingAccount

---
+++ P02_CheckingAccount/CheckingAccountOriginal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02_CheckingAccount
+{
+    public class CheckingAccountOriginal
+    {
+        public int AccountNumber { get; set; }
+        public string CustomerName { get; set; }
+        public string Email { get; private set; }
+
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+
+        public string SocialSecurityNumber { get; set; }
+        public DateTime ActiveDate { get; set; }
+        public string GetSSNLast4Digits()
+        {
+            int index = SocialSecurityNumber.LastIndexOf("-", StringComparison.Ordinal);
+            return index > 0 && index < SocialSecurityNumber.Length
+                ? SocialSecurityNumber.Substring(index + 1, SocialSecurityNumber.Length)
+                : SocialSecurityNumber;
+        }
+
+        public CheckingAccountOriginal(int accountNumber, string customerName, string email, string address, int zipCode, string city, string state, string country, string socialSecuriyNumber, DateTime activeDate)
+        {
+            AccountNumber = accountNumber;
+            CustomerName = customerName;
+            Email = email;
+            Address = address;
+            ZipCode = zipCode;
+            City = city;
+            State = state;
+            Country = country;
+            SocialSecurityNumber = socialSecuriyNumber;
+            ActiveDate = activeDate;
+        }
+    }
+}
---
+++ P02_CheckingAccount/CheckingAccountRefactored.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02_CheckingAccount
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string Email { get; private set; }
+    }
+
+    public class Address 
+    {
+        public int ZipCode { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+
+    public class SocialSecurity 
+    {
+        public string SocialSecurityNumber { get; set; }
+
+        public string GetSSNLast4Digits()
+        {
+            int index = SocialSecurityNumber.LastIndexOf("-", StringComparison.Ordinal);
+            return index > 0 && index < SocialSecurityNumber.Length
+                ? SocialSecurityNumber.Substring(index + 1, SocialSecurityNumber.Length)
+                : SocialSecurityNumber;
+        }
+    }
+
+    public class CheckingAccountRefactored
+    {
+        public int AccountNumber { get; set; }
+        public Customer Customer { get; set; }
+        public Address Address { get; set; }
+        public DateTime ActiveDate { get; set; }
+        public SocialSecurity SocialSecurity { get; set; }
+
+        public CheckingAccountRefactored(int accountNumber, DateTime activeDate)
+        {
+            AccountNumber = accountNumber;
+            ActiveDate = activeDate;
+        }
+
+        public CheckingAccountRefactored(int accountNumber, DateTime activeDate, Customer customer)
+            : this(accountNumber, activeDate)
+        {
+            Customer = customer;
+        }
+
+        public CheckingAccountRefactored(int accountNumber, DateTime activeDate, Customer customer, Address address)
+            : this(accountNumber, activeDate, customer)
+        {
+            Address = address;
+        }
+
+        public CheckingAccountRefactored(int accountNumber, Customer customer, Address address, DateTime activeDate, SocialSecurity socialSecurity)
+            : this(accountNumber, activeDate, customer, address)
+        {
+            SocialSecurity = socialSecurity;
+        }
+    }
+}
