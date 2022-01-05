f85b877c2e3c42c092577dce2dd6b32b95c04b3b
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2022-01-05 20:17:18 +0200

    Added the fifth problem - PhoneFormatter

---
+++ P05_PhoneFormatter/PhoneFormatterOriginal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P05_PhoneFormatter
+{
+    public class PhoneFormatterOriginal
+    {
+        public class Phone 
+        {
+            private string unformattedNumber;
+
+            public Phone(string unformattedNumber)
+            {
+                this.unformattedNumber = unformattedNumber;
+            }
+
+            public string GetAreaCode() 
+            {
+                string areaCode = unformattedNumber.Substring(0, 4);
+                return areaCode;
+            }
+
+            public string GetPrefix() 
+            {
+                string prefix = unformattedNumber.Substring(4, 2);
+                return prefix;
+            }
+
+            public string GetMiddleDigits() 
+            {
+                string middleDigits = unformattedNumber.Substring(6, 3);
+                return middleDigits;
+            }
+
+            public string GetLastDigits() 
+            {
+                string lastDigits = unformattedNumber.Substring(9, 4);
+                return lastDigits;
+            }
+        }
+
+        public class Customer 
+        {
+            private Phone phone;
+
+            public Customer(Phone phone)
+            {
+                this.phone = phone;
+            }
+
+            public void GetPhoneNumber() 
+            {
+                string formattedNumber =  $"{phone.GetAreaCode()} {phone.GetPrefix()} {phone.GetMiddleDigits()} {phone.GetLastDigits()}";
+                Console.WriteLine(formattedNumber);
+            }
+        }
+    }
+}
---
+++ P05_PhoneFormatter/PhoneFormatterRefactored.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P05_PhoneFormatter
+{
+    public class PhoneFormatterRefactored
+    {
+        public class Phone 
+        {
+            private string unformattedNumber;
+
+            public Phone(string unformattedNumber)
+            {
+                this.unformattedNumber = unformattedNumber;
+            }
+
+            public string GetAreaCode()
+            {
+                string areaCode = unformattedNumber.Substring(0, 4);
+                return areaCode;
+            }
+
+            public string GetPrefix()
+            {
+                string prefix = unformattedNumber.Substring(4, 2);
+                return prefix;
+            }
+
+            public string GetMiddleDigits()
+            {
+                string middleDigits = unformattedNumber.Substring(6, 3);
+                return middleDigits;
+            }
+
+            public string GetLastDigits()
+            {
+                string lastDigits = unformattedNumber.Substring(9, 4);
+                return lastDigits;
+            }
+
+            public string FormatPhoneNumber() 
+            {
+                string formattedNumber = $"{GetAreaCode()} {GetPrefix()} {GetMiddleDigits()} {GetLastDigits()}";
+                return formattedNumber;
+            }
+        }
+
+        public class Customer 
+        {
+            private Phone phone;
+
+            public Customer(Phone phone)
+            {
+                this.phone = phone;
+            }
+
+            public void GetPhoneNumber() 
+            {
+                Console.WriteLine(phone.FormatPhoneNumber());
+            }
+        }
+    }
+}
